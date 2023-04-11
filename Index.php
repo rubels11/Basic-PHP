e92b4eac87e019f7e4b69dc049d6106497b81668
Mahmud Hasan <32523771+rubels11@users.noreply.github.com>
2023-04-11 21:40:54 +0600

    Update Index.php

--- Index.php
+++ Index.php
@@ -14,11 +14,21 @@ $array = [
 
 print_r($array);
 
+
+// While loop
 $var = 1;
 while($var <= 10) {
 echo $var . 'x5=' . $var * 57 . '<br/>';
 $var++;
 
+}
+
+
+// For Loop
+for ($var=1; $var<=10; $var++)
+
+{
+    echo $var . 'x5=' . $var * 9 . '<br/>';
 }
 
 
