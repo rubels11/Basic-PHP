bbff9049e50e40bc65bb6a606c6a118ff3fd2b56
Mahmud Hasan <32523771+rubels11@users.noreply.github.com>
2023-04-11 21:29:46 +0600

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,25 @@
+<?php 
+
+$array = [
+
+    'name'=> "Mahmud",
+    'age'=> 25,
+    'address' => [
+
+        'street'=>'House No',
+        'city'=> 'Dhaka'
+    ]
+    
+    ];
+
+print_r($array);
+
+$var = 1;
+while($var <= 10) {
+echo $var . 'x5=' . $var * 57 . '<br/>';
+$var++;
+
+}
+
+
+?>
